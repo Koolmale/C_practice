9da80536e057ee6d4bc1fb49119fadc1486a0079
Koolmale <62354814+Koolmale@users.noreply.github.com>
2021-01-03 16:11:22 -0600

    enumerating constant values

---
+++ enumerating.c
@@ -0,0 +1,21 @@
+/*The enum keyword allows to create a clean and concise sequence of integers, and unless specified the sequence always starts with 0*/
+
+#include <stdio.h>
+
+int main () {
+  enum snooker
+    {RED=1, YELLOW, GREEN, BROWN, BLUE, PINK, BLACK};
+
+  int total;
+
+  printf("\n I potted a red worth %d \n", RED);
+  printf("Then a black worth %d \n", BLACK);
+  printf("Followed by another red worth %d \n", RED);
+  printf("And finally a blue worth %d \n", BLUE);
+
+  total = RED + BLACK + RED + BLUE;
+
+  printf("\n Altogether I scored %d \n", total);
+
+  return 0;
+}
