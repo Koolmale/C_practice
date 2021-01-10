af0e4ad5a669cc9e66d9725b319b5390cb26ea36
koolmale <kumel.h61110@gmail.com>
2021-01-10 13:45:11 -0600

    conditional operator

---
+++ conditions.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+    int num = 6;
+
+    (num % 2 != 0) ? printf("number is odd, %d \n", num) : printf("number is even, %d \n", num);
+
+    printf("There %s", (num ==1) ? "is" : "are");
+    printf("%d %s \n", num, (num ==1) ? "apple" : "apples");
+
+    num = 1;
+    printf("There %s ", (num ==1) ? "is" : "are");
+    printf("%d %s \n", num, (num==1) ? "apple" : "apples");
+    return 0;
+}
