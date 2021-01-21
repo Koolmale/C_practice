bbd8555e12438cb0e3a4c11b4281ce46506376db
koolmale <kumel.h61110@gmail.com>
2021-01-20 18:16:26 -0600

    header files

---
+++ header_files.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "utils.h"
+
+void getnum() ;
+
+int main () {
+    getnum();
+    return 0;
+}
+
+void getnum() {
+    int num;
+    char again;
+
+    printf("Enter a number to be squared: ");
+    scanf("%i", &num);
+
+    printf("%i squared is %i \n", num, square(num));
+
+    printf("Square another number? Y or N: ");
+    scanf("%1s", &again);
+
+    if (again == "Y") || (again == 'y') getnum();
+    else return;
+}
+
